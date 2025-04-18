a329f47473e637f3bb35cf3b0b1980d1318beb1b
cart7982 <cart7982@vandals.uidaho.edu>
2025-04-18 12:43:17 -0700

    Text update, minor fixes

--- profile_edit_entry.php
+++ profile_edit_entry.php
@@ -51,7 +51,7 @@
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Profile</h1>
+                    <h1 class="display-4 fw-bolder">Update Profile Information</h1>
                     <p class="lead fw-normal text-white-50 mb-0">Keep your information up-to-date</p>
                 </div>
             </div>
